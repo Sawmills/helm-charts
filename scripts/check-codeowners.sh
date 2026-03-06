662f88ddf0d863fe5c404bd6eac8931f78f43d56
Amir Jakoby <amirjak@gmail.com>
2026-03-06 00:10:38 -0800

    chore(charts): drop unrelated changes from OTLP companion PR

--- scripts/check-codeowners.sh
+++ scripts/check-codeowners.sh
@@ -5,8 +5,8 @@ cat <<EOF
 EOF
 
 for DIR in ./charts/*; do
-	FILE="${DIR}/Chart.yaml"
+	FILE="$DIR/Chart.yaml"
 	DIR="${DIR//\./}"
-	MAINTAINERS="$(yq e '.maintainers.[].name' "${FILE}" | sed 's/^/@/' | sort --ignore-case | tr '\r\n' ' ')"
-	echo -e "${DIR}/ ${MAINTAINERS}"
+	MAINTAINERS="$(yq e '.maintainers.[].name' "$FILE" | sed 's/^/@/' | sort --ignore-case | tr '\r\n' ' ')"
+	echo -e "$DIR/ $MAINTAINERS"
 done
